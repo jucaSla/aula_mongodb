4408c656d9beabc881573cfbb8c85bd3305b3058
JucaSla <jaomanuelb@gmail.com>
2023-05-10 16:35:36 -0300

    Referenciando os posts do usuario

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "acordei feliz hoje",new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		repository.save(maria);
 	}
 
 }
