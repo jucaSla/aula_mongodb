4149648a66fbafcc27b962499de569f3a33631e2
JucaSla <jaomanuelb@gmail.com>
2023-05-26 14:37:08 -0300

    query methods

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -43,8 +43,11 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "acordei feliz hoje",new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 		
-		post1.getComment().add(c1);
+		post1.getComment().addAll(Arrays.asList(c1,c2));
+		post2.getComment().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
